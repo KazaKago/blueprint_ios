5faddbd93c284f6e09097384517aa5eff4dfb2f6
Kensuke Tamura <kazakago@gmail.com>
2021-04-25 22:58:11 +0900

    Initial Commit

---
+++ Blueprint/BlueprintApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlueprintApp.swift
+//  Blueprint
+//
+//  Created by Kensuke Tamura on 2021/04/25.
+//
+
+import SwiftUI
+
+@main
+struct BlueprintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Blueprint/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Blueprint
+//
+//  Created by Kensuke Tamura on 2021/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
