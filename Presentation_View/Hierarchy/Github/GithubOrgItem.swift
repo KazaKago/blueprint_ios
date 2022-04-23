2156fee99b79570ef5ec7c858afc25182157f733
Kensuke Tamura <kazakago@gmail.com>
2022-04-24 03:38:15 +0900

    add avatar image

--- Presentation_View/Hierarchy/Github/GithubOrgItem.swift
+++ Presentation_View/Hierarchy/Github/GithubOrgItem.swift
@@ -13,13 +13,24 @@ struct GithubOrgItem: View {
     let githubOrg: GithubOrg
 
     var body: some View {
-        VStack(alignment: .leading) {
-            Text(String(format: "id".localized, githubOrg.id.value))
-                .font(.caption)
+        HStack {
+            AsyncImage(url: githubOrg.imageUrl) { image in
+                image.resizable().scaledToFit()
+            } placeholder: {
+                ProgressView()
+            }
+            .frame(width: 40, height: 40)
+            .cornerRadius(4)
             Spacer()
-                .frame(height: 4)
-            Text(githubOrg.name.value)
-                .font(.body)
+                .frame(width: 16)
+            VStack(alignment: .leading) {
+                Text(String(format: "id".localized, githubOrg.id.value))
+                    .font(.caption)
+                Spacer()
+                    .frame(height: 4)
+                Text(githubOrg.name.value)
+                    .font(.body)
+            }
         }
         .padding(EdgeInsets(top: 8, leading: 16, bottom: 8, trailing: 16))
     }
