cc2a0e388054d2e07d3d2b61e86c795ed249afb5
Kensuke Tamura <kazakago@gmail.com>
2022-04-24 11:45:45 +0900

    wip

--- Domain_Model/Hierarchy/About/Email.swift
+++ Domain_Model/Hierarchy/About/Email.swift
@@ -8,9 +8,16 @@
 import Foundation
 
 public struct Email {
+
+    static let REGEX_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$"
+
     public let value: String
 
     public init(_ value: String) {
+//        let regex = try NSRegularExpression(pattern: Self.REGEX_PATTERN)
+//        guard let _ = regex.firstMatch(in: value, range: NSRange(location: 0, length: value.count)) else {
+//            fatalError()
+//        }
         self.value = value
     }
 
